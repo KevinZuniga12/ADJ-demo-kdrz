5ab07024b19ef020faa2bfaa8ef7d451275fe860
KevinZuniga12 <20223tn108@utez.edu.mx>
2025-10-15 16:19:51 -0600

    feat: Update application configuration and structure with Docker support, Bootstrap integration, and API controller

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import TestController from './modules/test/test.controller'
 
 
 function App() {
@@ -5,7 +8,24 @@ function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" >ADJ-DEMO</a>
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container mt-4">
+        <h2>Inicio</h2>
+        <hr />
+        <button onClick={() => TestController.callToApi()} className="btn btn-success">Llama a mi Api</button>
+      </div>
     </>
   )
 }
